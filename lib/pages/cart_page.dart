7e534e738b3584e253d7fba650a126a75cbb342b
Jeron <norejudeisa@gmail.com>
2023-09-09 15:33:11 +0000

    refactor cart page

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,6 +1,5 @@
-import 'dart:async';
-
 import 'package:agriplant/data/products.dart';
+import 'package:agriplant/widgets/cart_item.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_iconly/flutter_iconly.dart';
 
@@ -16,141 +15,48 @@ class _CartPageState extends State<CartPage> {
 
   @override
   Widget build(BuildContext context) {
+    final total =
+        cartItems.map((cartItem) => cartItem.price).reduce((value, element) => value + element).toStringAsFixed(2);
     return Scaffold(
-      body: ListView.separated(
+      body: SingleChildScrollView(
         padding: const EdgeInsets.all(16),
-        itemCount: cartItems.length,
-        separatorBuilder: (__, _) => const SizedBox(height: 10),
-        itemBuilder: (context, index) {
-          final cartItem = cartItems[index];
-          return Dismissible(
-            key: UniqueKey(),
-            direction: DismissDirection.endToStart,
-            background: Container(
-              alignment: Alignment.centerRight,
-              padding: const EdgeInsets.only(right: 20),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.red,
-              ),
-              child: const Icon(
-                IconlyLight.delete,
-                color: Colors.white,
-                size: 25,
-              ),
+        child: Column(
+          children: [
+            ...List.generate(
+              cartItems.length,
+              (index) {
+                final cartItem = cartItems[index];
+                return Padding(
+                  padding: const EdgeInsets.only(bottom: 5),
+                  child: CartItem(cartItem: cartItem),
+                );
+              },
             ),
-            confirmDismiss: (DismissDirection direction) async {
-              final completer = Completer<bool>();
-              ScaffoldMessenger.of(context).showSnackBar(
-                SnackBar(
-                  action: SnackBarAction(
-                    label: "Keep",
-                    onPressed: () {
-                      completer.complete(false);
-                      ScaffoldMessenger.of(context).removeCurrentSnackBar();
-                    },
-                  ),
-                  content: const Text(
-                    "Remove from cart?",
-                  ),
-                ),
-              );
-              Timer(const Duration(seconds: 4), () {
-                if (!completer.isCompleted) {
-                  completer.complete(true);
-                  ScaffoldMessenger.of(context).removeCurrentSnackBar();
-                }
-              });
-
-              return await completer.future;
-            },
-            child: SizedBox(
-              height: 125,
-              child: Card(
-                clipBehavior: Clip.antiAlias,
-                shape: RoundedRectangleBorder(
-                  borderRadius: const BorderRadius.all(Radius.circular(10)),
-                  side: BorderSide(color: Colors.grey.shade200),
-                ),
-                elevation: 0.1,
-                child: Padding(
-                  padding: const EdgeInsets.all(10),
-                  child: Row(
-                    children: [
-                      Container(
-                        height: double.infinity,
-                        width: 90,
-                        margin: const EdgeInsets.only(right: 15),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          image: DecorationImage(
-                            fit: BoxFit.cover,
-                            image: AssetImage(cartItem.image),
-                          ),
-                        ),
+            const SizedBox(height: 15),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("Total (${cartItems.length} items)"),
+                Text(
+                  "\$$total",
+                  style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                        color: Theme.of(context).colorScheme.primary,
+                        fontWeight: FontWeight.bold,
                       ),
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(cartItem.name, style: Theme.of(context).textTheme.titleMedium),
-                            const SizedBox(height: 2),
-                            Text(
-                              cartItem.description,
-                              style: Theme.of(context).textTheme.bodySmall,
-                              overflow: TextOverflow.ellipsis,
-                              maxLines: 2,
-                            ),
-                            const Spacer(),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Text(
-                                  "\$${cartItem.price}",
-                                  style: Theme.of(context).textTheme.titleMedium?.copyWith(
-                                        color: Theme.of(context).colorScheme.primary,
-                                      ),
-                                ),
-                                SizedBox(
-                                  height: 30,
-                                  child: ToggleButtons(
-                                    borderRadius: BorderRadius.circular(99),
-                                    constraints: const BoxConstraints(
-                                      minHeight: 30,
-                                      minWidth: 30,
-                                    ),
-                                    selectedColor: Theme.of(context).colorScheme.primary,
-                                    isSelected: const [
-                                      true,
-                                      false,
-                                      true,
-                                    ],
-                                    children: const [
-                                      Icon(
-                                        Icons.remove,
-                                        size: 20,
-                                      ),
-                                      Text("2"),
-                                      Icon(
-                                        Icons.add,
-                                        size: 20,
-                                      ),
-                                    ],
-                                    onPressed: (int index) {},
-                                  ),
-                                ),
-                              ],
-                            )
-                          ],
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              ),
+                )
+              ],
             ),
-          );
-        },
+            const SizedBox(height: 20),
+            SizedBox(
+              width: double.infinity,
+              child: FilledButton.icon(
+                onPressed: () {},
+                label: const Text("Proceed to Checkout"),
+                icon: const Icon(IconlyBold.arrowRight),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
---
+++ lib/widgets/cart_item.dart
@@ -0,0 +1,144 @@
+import 'dart:async';
+
+import 'package:agriplant/models/product.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_iconly/flutter_iconly.dart';
+
+class CartItem extends StatelessWidget {
+  const CartItem({super.key, required this.cartItem});
+
+  final Product cartItem;
+
+  @override
+  Widget build(BuildContext context) {
+    return Dismissible(
+      key: UniqueKey(),
+      direction: DismissDirection.endToStart,
+      background: Container(
+        alignment: Alignment.centerRight,
+        padding: const EdgeInsets.only(right: 20),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: Colors.red,
+        ),
+        child: const Icon(
+          IconlyLight.delete,
+          color: Colors.white,
+          size: 25,
+        ),
+      ),
+      confirmDismiss: (DismissDirection direction) async {
+        final completer = Completer<bool>();
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            duration: const Duration(seconds: 3),
+            action: SnackBarAction(
+              label: "Keep",
+              onPressed: () {
+                completer.complete(false);
+                ScaffoldMessenger.of(context).removeCurrentSnackBar();
+              },
+            ),
+            content: const Text(
+              "Remove from cart?",
+            ),
+          ),
+        );
+        Timer(const Duration(seconds: 3), () {
+          if (!completer.isCompleted) {
+            completer.complete(true);
+            ScaffoldMessenger.of(context).removeCurrentSnackBar();
+          }
+        });
+
+        return await completer.future;
+      },
+      child: SizedBox(
+        height: 125,
+        child: Card(
+          clipBehavior: Clip.antiAlias,
+          shape: RoundedRectangleBorder(
+            borderRadius: const BorderRadius.all(Radius.circular(10)),
+            side: BorderSide(color: Colors.grey.shade200),
+          ),
+          elevation: 0.1,
+          child: Padding(
+            padding: const EdgeInsets.all(10),
+            child: Row(
+              children: [
+                Container(
+                  height: double.infinity,
+                  width: 90,
+                  margin: const EdgeInsets.only(right: 15),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: AssetImage(cartItem.image),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(cartItem.name, style: Theme.of(context).textTheme.titleMedium),
+                      const SizedBox(height: 2),
+                      Text(
+                        cartItem.description,
+                        style: Theme.of(context).textTheme.bodySmall,
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 2,
+                      ),
+                      const Spacer(),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "\$${cartItem.price}",
+                            style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                                  color: Theme.of(context).colorScheme.primary,
+                                ),
+                          ),
+                          SizedBox(
+                            height: 30,
+                            child: ToggleButtons(
+                              borderRadius: BorderRadius.circular(99),
+                              constraints: const BoxConstraints(
+                                minHeight: 30,
+                                minWidth: 30,
+                              ),
+                              selectedColor: Theme.of(context).colorScheme.primary,
+                              isSelected: const [
+                                true,
+                                false,
+                                true,
+                              ],
+                              children: const [
+                                Icon(
+                                  Icons.remove,
+                                  size: 20,
+                                ),
+                                Text("2"),
+                                Icon(
+                                  Icons.add,
+                                  size: 20,
+                                ),
+                              ],
+                              onPressed: (int index) {},
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+    ;
+  }
+}
